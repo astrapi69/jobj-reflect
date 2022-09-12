64e14f3075f1b5eff4a89c80159b830141836bbd
astrapi69 <asterios.raptis@gmx.net>
2022-09-12 19:37:46 +0200

    initial version

---
+++ src/main/java/io/github/astrapi69/reflection/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains utility classes for the reflection
+ */
+package io.github.astrapi69.reflection;
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module jobj.reflect.main {
+	requires jobj.core.main;
+	requires lombok;
+	requires org.objenesis;
+	requires java.logging;
+	exports io.github.astrapi69.reflection;
+}
